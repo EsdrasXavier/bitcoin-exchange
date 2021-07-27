347fe773927d9fc9a676c5b40e361a4a4a065d11
Esdras Xavier <esdrasxa@gmail.com>
2021-07-27 19:46:36 -0300

    Created the project.

---
+++ src/test/java/com/example/bitcoin/exchange/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bitcoin.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
